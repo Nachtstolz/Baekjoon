34560a0f3552b5f768a9803b780897dd4e8c7d33
Nachtstolz <sayong4424@gmail.com>
2024-02-18 01:02:14 +0900

    240218 :: completely solved problem no.2609 is pushed - commit msg of commit-id.b36705e should be substituted for this msg

--- BRONZE I/2609.py
+++ BRONZE I/2609.py
@@ -7,7 +7,7 @@
 
 a, b = map(int, input().split())
 
-std = min(a, b)
+std = min(a, b) # 작은 수 출력
 res1 = 0 # 최대공약수
 res2 = 0 # 최소공배수
 
@@ -15,7 +15,6 @@ for i in range(std, 0, -1) :
     if a % i == 0 and b % i == 0 :
         res1 = i
         break
-
 res2 = res1 * (a//res1) * (b//res1)
 
 print(res1)

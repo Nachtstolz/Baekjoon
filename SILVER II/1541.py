6125e8082b30e9628ce447a435e05a14c75e798a
Nachtstolz <sayong4424@gmail.com>
2024-09-05 16:58:01 +0900

    240905 :: the problem no.1541 is solved with some search on the website about solution algo

--- SILVER II/1541.py
+++ SILVER II/1541.py
@@ -1,5 +1,6 @@
-# 1541
+# 1541 # 그리디 # 파싱 # 인터넷 자료 서치
 # 잃어버린 괄호
+
 # 괄호를 적절히 쳐서 이 식의 값을 최소로 만드는 프로그램을 작성하시오.
 # 첫째 줄에 식이 주어진다. 식은 '0~9', '+' 그리고 '-'만으로 이루어져있고,
 # 가장 처음과 마지막 문자는 숫자이다.
@@ -7,12 +8,27 @@
 # 수는 0 으로 시작할 수 있다. 입력으로 주어지는 식의 길이는 50보다 작거나 같다
 
 #exp = list(input())
-exp = list(input())
-print(exp) # 디버깅
+first = input()
+exp = list(first)
+# print(exp) # 디버깅
 
-min = 0
+minus_li = first.split('-')
+res = 0
+for i in minus_li[0].split('+') :
+    res+=int(i)
+
+for each in minus_li[1:] :
+    for j in each.split('+') :
+        res-=int(j)
+
+print(res)
+# 인풋으로 받은 exp를 연산자와 숫자로 분리하는 과정
+'''
+minst = int(1e9)
 num = []
 li = []
+# oper_cnt = 0
+# num_cnt = 0
 
 for i in range(len(exp)) :
     print('i : ', i) # 디버깅
@@ -20,11 +36,12 @@ for i in range(len(exp)) :
     
     if exp[i] == '+' or exp[i] == '-' :
         del num[-1]
+        # oper_cnt+=1
         
         num_str = ''
         for j in range(len(num)) :
             num_str += num[j]
-        li.append(int(num_str))
+        li.append(str(int(num_str)))
         
         print('num : ', num) # 디버깅
         print('li : ', li) # 디버깅
@@ -37,11 +54,35 @@ for i in range(len(exp)) :
         num_str = ''
         for j in range(len(num)) :
             num_str += num[j]
-        li.append(int(num_str))
-    print(li)
+        li.append(str(int((num_str))))
+    print(li) # 디버깅
     
-    
-    
+    # num_cnt = len(li)-oper_cnt
+'''
 
-    
+# 작은 괄호 -> 큰 괄호로 늘려가는 방식
+'''
+for i in range(0, len(li)-2,2) :
+    li.insert(i, '(')
+    for j in range(i+4, len(li)+1, 2) :
+        li.insert(j, ')')
+        print(li) # 디버깅
+        tmp = ''.join(li)
+        print(tmp) # 디버깅
+        li.remove(')')
+        minst = min(minst, eval(tmp))
+        # print(minst)
+    li.remove('(')
+'''
 
+# 큰 괄호 -> 작은 괄호로 줄여가는 방식
+# 처음에 가장 큰 괄호 만들고 시작
+'''
+li.insert(0, '(')
+li.append(')')
+minst = min(minst, ''.join(li))
+
+for i in range(1, num_cnt) :
+    idx = 2
+    while 
+'''
